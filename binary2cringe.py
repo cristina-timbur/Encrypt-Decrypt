c7f8b1153491dff94e85a98c7fe941aca86f3a6e
cristina-timbur <82144567+cristina-timbur@users.noreply.github.com>
2021-12-07 17:11:12 +0200

    spatierea

--- binary2cringe.py
+++ binary2cringe.py
@@ -1,8 +1,9 @@
 def normal(string):
     val = 0
     for i in range(len(string)):
-        val += int(string[i])*(2**(7-i))
+        val += int(string[i]) * (2 ** (7 - i))
     return chr(int(val))
+
 ff = open("output", "w")
 with open("output.txt", "r") as f:
     text = f.read()
